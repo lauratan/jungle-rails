036235e20e646ad70ef2bce38304ae3234f96f5f
lauratan <laura.tan108@gmail.com>
2018-07-19 20:48:22 +0000

    Added order details after payment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,8 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    # @line_items = LineItem.where(order_id: @order.id)
+    # @product = Product.find_by(id: line_items.product_id)
   end
 
   def create
