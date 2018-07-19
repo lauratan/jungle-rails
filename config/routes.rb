27013fd33f89cff8cccc62d5116bff2ccc24f2f3
lauratan <laura.tan108@gmail.com>
2018-07-19 16:04:14 +0000

    Fixed logout routes using resources

--- config/routes.rb
+++ config/routes.rb
@@ -20,12 +20,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  #working routes: 
-  resources :sessions, only: [:new, :create]
-  get '/logout' => 'sessions#destroy'
-
-  #testing routes:
-  # resources :sessions, only: [:new, :create, :destroy]
+  resources :sessions, only: [:new, :create, :destroy]
 
 
   # get '/login' => 'sessions#new'
